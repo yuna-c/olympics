14583d97af91f0e3a6924a34832ab34601a1e5b2
yuna <yuna744@gmail.com>
2024-08-21 18:08:52 +0900

    📝 add : delete

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,42 @@ import './styles/App.css';
 
 function App() {
   const [countrise, setcountrise] = useState([]);
-
   const [countryInput, setCountryInput] = useState('');
   const [goldInput, setGoldInput] = useState(0);
   const [silverInput, setSilverInput] = useState(0);
   const [bronzeInput, setBronzeInput] = useState(0);
 
-  const addCountry = (e) => {
-    e.preventDefault();
-    const newCountry = {
-      // id: new Date().getTime(),
-      name: countryInput,
-      gold: goldInput,
-      silver: silverInput,
-      bronze: bronzeInput
-    };
-    // 기존 나라들과 새로운 나라들을 추가 하기 위해 의존성 배열
-    setcountrise([...countrise, newCountry]);
-  };
-
-  const updataCountry = (e) => {
-    e.preventDefault(); // 버블링방지(중복 생김)
-
-    const existingCountry = countrise.find((country) => {
+  const checkExistance = () => {
+    return countrise.find((country) => {
       if (country.name.toLowerCase() === countryInput.toLowerCase()) {
         return true;
       } else {
         return false;
       }
     });
+  };
 
-    if (existingCountry) {
+  const addCountry = (e) => {
+    e.preventDefault();
+
+    if (checkExistance()) {
+      alert('이미 등록된 국가입니다.');
+    } else {
+      const newCountry = {
+        name: countryInput,
+        gold: goldInput,
+        silver: silverInput,
+        bronze: bronzeInput
+      };
+      // 기존 나라들과 새로운 나라들을 추가 하기 위해 의존성 배열
+      setcountrise([...countrise, newCountry]);
+    }
+  };
+
+  const updataCountry = (e) => {
+    e.preventDefault(); // 버블링방지(중복 생김)
+
+    if (checkExistance()) {
       // existingCountry===  true는 왜 입력이 안되고 else로 빠질까
       setcountrise(
         countrise.map((country) => {
@@ -50,7 +55,22 @@ function App() {
       alert('등록되지 않은 국가입니다');
     }
   };
-  console.log(countrise);
+
+  // 이름을 받아 같은 이름을 삭제
+  const deleteCountry = (name) => {
+    setcountrise(
+      countrise.filter((country) => {
+        if (country.name.toLowerCase() === name) {
+          return null;
+        } else {
+          return country;
+        }
+      })
+    );
+
+    alert(`${name}이 삭제되었습니다`);
+  };
+
   return (
     <>
       {/* <div className='cursor-wrapper'>
@@ -99,6 +119,14 @@ function App() {
                     <td>{country.gold}</td>
                     <td>{country.silver}</td>
                     <td>{country.bronze}</td>
+                    <td
+                      className="delete-btn"
+                      onClick={() => {
+                        deleteCountry(country.name);
+                      }}
+                    >
+                      삭제
+                    </td>
                   </tr>
                 );
               })}
@@ -109,4 +137,5 @@ function App() {
     </>
   );
 }
+
 export default App;
